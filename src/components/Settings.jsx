610e0767144d27a7d504d794d98eabfa7eddc4dc
Shubham <shubhamkulkarni.sh@gmail.com>
2023-01-21 13:30:44 +0530

    fix settings

---
+++ src/components/Settings.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import $ from '../themes/constants'
+import prettyLight from '../assets/pretty.svg'
+import prettyDark from '../assets/pretty-dark.svg'
+import dark from '../assets/dark.svg';
+import light from '../assets/light.svg';
+
+function Settings({saved,theme,darkLayoutSrc,lightLayoutSrc,btnTitle,toggleTheme,onLayoutChange,onPrettify}) {
+  return (
+    <div className="icon-btns flex items-center">
+          {saved && <div className="text-xs mr-4 text-blue-700 dark:text-neutral-200">Changes saved !</div>}
+          
+          <button
+            title={"Change layout"}
+            className="m-0 rounded-full p-1 sm:block  hover:bg-neutral-300 dark:hover:bg-neutral-600 hidden"
+            type="button"
+            onClick={onLayoutChange}
+          >
+            <img src={theme === $.DARK ? darkLayoutSrc : lightLayoutSrc} className="dark-icon h-5" />
+          </button>
+          
+          <button
+            title={"Prettify your code"}
+            className="m-0 ml-2 rounded-full p-1  hover:bg-neutral-300 dark:hover:bg-neutral-600"
+            type="button"
+            onClick={onPrettify}
+          >
+            <img src={theme === $.DARK ? prettyLight : prettyDark} className="dark-icon h-5" />
+          </button>
+
+          <button
+            title={btnTitle}
+            className="m-0 ml-2 rounded-full p-1  hover:bg-neutral-300 dark:hover:bg-neutral-600"
+            type="button"
+            onClick={toggleTheme}
+          >
+            <img src={theme === $.DARK ? light : dark} className="dark-icon h-5" />
+          </button>
+
+        </div>
+  )
+}
+
+export default Settings
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,15 +1,9 @@
 import { useState } from 'react';
 import { useEditor } from '../engine/hooks';
-
-import dark from '../assets/dark.svg';
-import light from '../assets/light.svg';
-
 import CodeView from '../views/CodeView';
 import ResultsView from '../views/ResultView';
 import $ from '../themes/constants';
 import { useCallback } from 'react';
-import prettyLight from '../assets/pretty.svg'
-import prettyDark from '../assets/pretty-dark.svg'
 import {js_beautify} from 'js-beautify';
 import Storage from '../engine/storage';
 import { useEffect } from 'react';
@@ -18,6 +12,7 @@ import rowsLight from '../assets/rows.svg';
 import columnsLight from '../assets/columns.svg';
 import rowsDark from '../assets/rowsDark.svg';
 import columnsDark from '../assets/columnsDark.svg';
+import Settings from '../components/Settings';
 
 const storage = new Storage({type: localStorage, async: false});
 
@@ -87,7 +82,7 @@ const Main = () => {
     setSaved(false);
     setCodeString(val);
     debounceFn(val);
-  },[]);
+  },[debounceFn]);
 
   const btnTitle = theme === $.DARK ? 'Switch to light theme' : 'Switch to dark theme';
   
@@ -105,37 +100,15 @@ const Main = () => {
            <a href='https://github.com/shubhkulkarni' target={"_blank"}>_by_shubham</a> 
           </sub>
         </div>
-        <div className="icon-btns flex items-center">
-          {saved && <div className="text-xs mr-4 text-blue-700 dark:text-neutral-200">Changes saved !</div>}
-          
-          <button
-            title={"Change layout"}
-            className="m-0 rounded-full p-1 sm:block  hover:bg-neutral-300 dark:hover:bg-neutral-600 hidden"
-            type="button"
-            onClick={onLayoutChange}
-          >
-            <img src={theme === $.DARK ? darkLayoutSrc : lightLayoutSrc} className="dark-icon h-5" />
-          </button>
-          
-          <button
-            title={"Prettify your code"}
-            className="m-0 ml-2 rounded-full p-1  hover:bg-neutral-300 dark:hover:bg-neutral-600"
-            type="button"
-            onClick={onPrettify}
-          >
-            <img src={theme === $.DARK ? prettyLight : prettyDark} className="dark-icon h-5" />
-          </button>
 
-          <button
-            title={btnTitle}
-            className="m-0 ml-2 rounded-full p-1  hover:bg-neutral-300 dark:hover:bg-neutral-600"
-            type="button"
-            onClick={toggleTheme}
-          >
-            <img src={theme === $.DARK ? light : dark} className="dark-icon h-5" />
-          </button>
-
-        </div>
+        <Settings saved={saved} theme={theme} 
+            darkLayoutSrc={darkLayoutSrc}
+            lightLayoutSrc={lightLayoutSrc}
+            btnTitle={btnTitle}
+            toggleTheme={toggleTheme}
+            onLayoutChange={onLayoutChange}
+            onPrettify={onPrettify}
+          />
       </div>
 
       <div className={`work-space flex-column ${columnLayout ? `sm:flex` : `sm:flex-column`} w-full flex-1 items-center justify-between `}>
